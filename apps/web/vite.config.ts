b97e341160113b92bf73eb6061e5a37190124887
趙文傑 <wenj@wenj-3.local>
2025-12-25 16:34:14 +0800

    ci: add deployment workflow

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,8 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/ed-workstation/', // 確保這裡跟你的 Repo 名字一模一樣！
   plugins: [
     react(),
-    tailwindcss(), // 加入這一行
+    tailwindcss(),
   ],
 })
